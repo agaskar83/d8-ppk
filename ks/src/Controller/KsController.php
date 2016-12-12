b802d799b65cc0103452626f90d25669e4c83735
Nadeem <NadeemAhmed.Agaskar@pfizer.com>
2016-12-12 16:45:19 -0500

    update the links

--- ks/src/Controller/KsController.php
+++ ks/src/Controller/KsController.php
@@ -7,7 +7,11 @@
 
 namespace Drupal\ks\Controller;
 
+
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Link;
+use Drupal\Core\Url;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 class KsController extends ControllerBase{
     public function content() {
@@ -16,5 +20,11 @@ class KsController extends ControllerBase{
             '#markup' => $this->t('Kknowledge portal dashboard'),
         );
 }
+	public function settings() {
+		return array(
+			'#type'=>'markup',
+            '#markup' => $this->t('Kknowledge portal settings'),
+		);
+	}
 }
 
---
+++ ks/src/Plugin/Block/KsBlock.php
@@ -0,0 +1,30 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Drupal\ks\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a 'KS' Block
+ *
+ * @Block(
+ *   id = "ks_block",
+ *   admin_label = @Translation("KS block"),
+ * )
+ */
+class KsBlock extends BlockBase {
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    return array(
+      '#markup' => $this->t('KS block!'),
+    );
+  }
+}
